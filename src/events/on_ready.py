6e2f36dcee527e85330a5e3d83d537fa22693130
philipecerqueira <philipe.cerq@gmail.com>
2023-04-05 12:54:34 -0300

    feat: message to channel when bot ready

--- src/events/on_ready.py
+++ src/events/on_ready.py
@@ -1,6 +1,17 @@
 from src.config.discord import celestine
 
 
+def get_discord_channel():
+    for guild in celestine.guilds:
+        for channel in guild.text_channels:
+            return celestine.get_channel(channel.id)
+
+
 @celestine.event
 async def on_ready():
     print(f'Logged: {celestine.user}')
+
+    channel = get_discord_channel()
+
+    if channel is not None:
+        await channel.send('Estou online, precisar de mim é só chamar! ;D')
